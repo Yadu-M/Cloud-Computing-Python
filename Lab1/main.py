91689c5bea2dda7491a0187fbd9391432187eccd
Yadu-M <ymadhu@torontomu.ca>
2024-01-20 19:07:37 -0500

    finished 1.2

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,49 +1,90 @@
 import requests
 import json
+from time import perf_counter
+from threading import Thread
 from map import create_map, update_rover_path, create_rover_path, mine_check
 
-MAP_ROW_SIZE = 50
-MAP_COL_SIZE = 50
+MAP_ROW_SIZE = 20
+MAP_COL_SIZE = 20
 
 
 def main():
-
     create_map(MAP_ROW_SIZE, MAP_COL_SIZE)
 
+    time1 = single_thread()
+    time2 = multi_thread()
+    print(f'The difference is {round(abs(time1 - time2), 2)}')
+
+
+def single_thread():
+    start_time = perf_counter()
+    generate_rover_paths()
+    end_time = perf_counter()
+    print(f'Single threaded approach took {end_time - start_time: 0.2f} second(s) to complete.')
+    return end_time - start_time
+
+
+def multi_thread():
+    thread_list = []
+    start_time = perf_counter()
+
     for rover_id in range(1, 11):
-        rover_data = get_rover_commands(rover_id)
-        if mine_check(1, 0):
-            if rover_data['data']['moves'][0] != 'D':
-                print(f'bot{rover_id} explodes at (0, 0)')
-                continue
+        thread_list.append(Thread(target=generate_rover_path, args=(rover_id, )))
 
-        prev_move = ''
-        curr_row = 1
-        curr_col = 0
-        create_rover_path(rover_id, MAP_ROW_SIZE, MAP_COL_SIZE)
-        update_rover_path(rover_id, 1, 0)
-        for move in rover_data['data']['moves']:
-            if prev_move != 'D' and mine_check(curr_row, curr_col):
-                print(f'bot{rover_id} explodes at ({curr_row - 1}, {curr_col})')
-                break
+    for thread in thread_list:
+        thread.start()
 
-            if move == 'L':
-                if curr_col != 0:
-                    curr_col -= 1
+    for thread in thread_list:
+        thread.join()
 
-            elif move == 'R':
-                if curr_col != (MAP_COL_SIZE - 1):
-                    curr_col += 1
+    end_time = perf_counter()
+    print(f'Multi Threaded approach took {end_time - start_time: 0.2f} second(s) to complete.')
+    return end_time - start_time
 
-            elif move == 'M':
-                if curr_row != (MAP_ROW_SIZE - 1):
-                    curr_row += 1
 
-            update_rover_path(rover_id, curr_row, curr_col)
-            prev_move = move
+def generate_rover_paths():
+    """
+    Generates all the rover paths
+    :return: None
+    """
+    for rover_id in range(1, 11):  # 10 grabbing all 10 rover commands
+        generate_rover_path(rover_id)
+
+
+def generate_rover_path(rover_id):
+    rover_data = get_rover_commands(rover_id)
+    prev_move = ''
+    curr_row = 1
+    curr_col = 0
+    create_rover_path(rover_id, MAP_ROW_SIZE, MAP_COL_SIZE)
+    update_rover_path(rover_id, 1, 0)  # Initializing path
+    for move in rover_data['data']['moves']:
+        if prev_move != 'D' and mine_check(curr_row, curr_col):
+            # print(f'Rover {rover_id} exploded at ({curr_row}, {curr_col})')
+            break
+
+        if move == 'L':
+            if curr_col != 0:
+                curr_col -= 1
+
+        elif move == 'R':
+            if curr_col != (MAP_COL_SIZE - 1):
+                curr_col += 1
+
+        elif move == 'M':
+            if curr_row != (MAP_ROW_SIZE - 1):
+                curr_row += 1
+
+        update_rover_path(rover_id, curr_row, curr_col)
+        prev_move = move
 
 
 def get_rover_commands(rover_id):
+    """
+    Function grabs the rover commands from the api
+    :param rover_id: The rover id
+    :return: Rover commands in JSON
+    """
     api = 'https://coe892.reev.dev/lab1/rover'
     r = requests.get(f'{api}/{rover_id}')
     if r.ok:
--- Lab1/map.py
+++ Lab1/map.py
@@ -1,11 +1,17 @@
 import random
 
-"""
-Map function
-"""
+'''
+Utility functions for edition text files
+'''
 
 
 def create_map(row, col):
+    """
+    Creates a random map based on the row and col
+    :param row: Row size of map
+    :param col: Col size of map
+    :return:
+    """
     with open('map.txt', 'w') as f:
         f.write(f'{row} {col}\n')
         for i in range(row):
@@ -20,21 +26,30 @@ def create_map(row, col):
 
 
 def update_rover_path(rover_id, row, col):
-
+    """
+    Updates the rover path given the row and col
+    :param rover_id: Rover id
+    :param row: Row to update
+    :param col: Col to update
+    :return:
+    """
     rover_path = open(f'path_{rover_id}.txt', 'rb+')
-
-    # rover_path.seek(0, 0)
-    # print(curr_row)
-    for i in range(row):
+    for i in range(row):  # Align file pointer to correct row
         rover_path.readline()
 
-    rover_path.seek(col, 1)
-    rover_path.write(bytes('*', 'utf-8'))
+    rover_path.seek(col, 1)  # Align file pointer to correct col
+    rover_path.write(bytes('*', 'utf-8'))  # Marking rover path
     rover_path.close()
     return True
 
 
 def mine_check(row, col):
+    """
+    Checks if mine exists map for the given row, col
+    :param row: Row in the map
+    :param col: Col in the map
+    :return:
+    """
     rover_map = open('map.txt', 'rb+')
 
     for i in range(row):
@@ -42,9 +57,9 @@ def mine_check(row, col):
 
     rover_map.seek(col, 1)
 
-    if rover_map.read(1).decode('utf-8') == '1':
-        rover_map.seek(-1, 1)
-        rover_map.write(bytes('X', 'utf-8'))
+    if rover_map.read(1).decode('utf-8') == '1':  # Checking for mine
+        rover_map.seek(-1, 1)  # Move the pointer 1 back to reset
+        rover_map.write(bytes('X', 'utf-8'))  # Mark mine as X (for now)
         rover_map.close()
         return True
 
@@ -53,6 +68,13 @@ def mine_check(row, col):
 
 
 def create_rover_path(rover_id, rows, cols):
+    """
+    Initializes the map for each rover
+    :param rover_id:  id
+    :param rows: Total number of rows
+    :param cols: Total number of cols
+    :return:
+    """
     with open(f'path_{rover_id}.txt', 'w') as f:
         f.write('\n')
         for i in range(rows):
